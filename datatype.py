ea45b44ebcb02b86a217b291f946f4bbaa53ead9
RipBlueskyRH <ripblueskyrh@gmail.com>
2024-11-16 11:11:33 -0500

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,11 @@
+d=5
+print ("the type of d is", type(d))
+
+b=True
+print ("the type of b is", type(b))
+
+a=1.56
+print ("the type of a is", type(a))
+
+t="hello"
+print ("the type of t is", type(t))
---
+++ typecasting.py
@@ -0,0 +1,11 @@
+num="6.8"
+print(type(num))
+
+x=(float(num))
+print(type(x))
+
+p=(int(x))
+print(type(p))
+
+t=str(p)
+print(type(t))
